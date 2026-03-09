d9654b0a22c63e48b36a46892e60eececc21dae6
mrinmoyblr1 <mrinmoy.blr@gmail.com>
2026-03-09 17:10:44 -0400

    Biswas

---
+++ src/main/java/RestAssuredP001/Basics.java
@@ -0,0 +1,38 @@
+package RestAssuredP001;
+
+import io.restassured.RestAssured;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class Basics {
+
+    @Test
+    public void basics() {
+
+        // Validating AddPlace API
+
+        // given: All the input details
+        // when: Submit the API
+        // then: Validate the response
+        RestAssured.baseURI = "https://rahulshettyacademy.com/";
+        given().log().all().queryParam("key", "qaclick123").header("Content-Type", "application/json")
+                .body("{\n" +
+                        "    \"location\": {\n" +
+                        "        \"lat\": -38.383494,\n" +
+                        "        \"lng\": 33.427362\n" +
+                        "    },\n" +
+                        "    \"accuracy\": 50,\n" +
+                        "    \"name\": \"Mrinmoy Biswas\",\n" +
+                        "    \"phone_number\": \"(+91) 983 893 3937\",\n" +
+                        "    \"address\": \"World Cross Lane, World\",\n" +
+                        "    \"types\": [\n" +
+                        "        \"shoe park\",\n" +
+                        "        \"shop\"\n" +
+                        "    ],\n" +
+                        "    \"website\": \"http://google.com\",\n" +
+                        "    \"language\": \"French-IN\"\n" +
+                        "}").when().post("maps/api/place/add/json")
+                .then().log().all().assertThat().statusCode(200);
+    }
+}
