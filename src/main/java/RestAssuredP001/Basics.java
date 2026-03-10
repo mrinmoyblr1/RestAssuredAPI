3935297e2e7d1bbd0e958b662666f7d23966bd6c
mrinmoyblr1 <mrinmoy.blr@gmail.com>
2026-03-10 02:31:41 -0400

    BISWAS

--- src/main/java/RestAssuredP001/Basics.java
+++ src/main/java/RestAssuredP001/Basics.java
@@ -2,6 +2,7 @@ package RestAssuredP001;
 
 import Files.payLoad;
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
@@ -16,17 +17,24 @@ public class Basics {
         // given: All the input details
         // when: Submit the API
         // then: Validate the response
+        //Add Place -> Update Place with New Address -> Get Place to validate if the New Address is present in the response
         RestAssured.baseURI = "https://rahulshettyacademy.com/";
-        given().log().all().queryParam("key", "qaclick123")
+        String response = given().log().all().queryParam("key", "qaclick123")
                 .header("Content-Type", "application/json")
                 .body(payLoad.AddPlace())
                 .when().post("maps/api/place/add/json")
-                .then().log().all()
+                .then()
                 .assertThat().statusCode(200)
                 .body("scope", equalTo("APP"))
                 .body("status", equalTo("OK"))
-                .header("Server", "Apache/2.4.52 (Ubuntu)");
-        //Add Place -> Update Place with New Address -> Get Place to validate if the New Address is present in the response
+                .header("Server", "Apache/2.4.52 (Ubuntu)").extract().response().asString();
+
+        System.out.println(response);
+
+        // To parse the response
+        JsonPath js = new JsonPath(response);
+        String placeId = js.getString("place_id");
+        System.out.println(placeId);
 
     }
 }
