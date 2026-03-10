51ff8724b8239f794d3b06353abd6026c998cacf
mrinmoyblr1 <mrinmoy.blr@gmail.com>
2026-03-10 02:05:37 -0400

    BISWAS

--- src/main/java/RestAssuredP001/Basics.java
+++ src/main/java/RestAssuredP001/Basics.java
@@ -1,5 +1,6 @@
 package RestAssuredP001;
 
+import Files.payLoad;
 import io.restassured.RestAssured;
 import org.testng.annotations.Test;
 
@@ -18,26 +19,14 @@ public class Basics {
         RestAssured.baseURI = "https://rahulshettyacademy.com/";
         given().log().all().queryParam("key", "qaclick123")
                 .header("Content-Type", "application/json")
-                .body("{\n" +
-                        "    \"location\": {\n" +
-                        "        \"lat\": -38.383494,\n" +
-                        "        \"lng\": 33.427362\n" +
-                        "    },\n" +
-                        "    \"accuracy\": 50,\n" +
-                        "    \"name\": \"Mrinmoy Biswas\",\n" +
-                        "    \"phone_number\": \"(+91) 983 893 3937\",\n" +
-                        "    \"address\": \"World Cross Lane, World\",\n" +
-                        "    \"types\": [\n" +
-                        "        \"shoe park\",\n" +
-                        "        \"shop\"\n" +
-                        "    ],\n" +
-                        "    \"website\": \"http://google.com\",\n" +
-                        "    \"language\": \"French-IN\"\n" +
-                        "}").when().post("maps/api/place/add/json")
+                .body(payLoad.AddPlace())
+                .when().post("maps/api/place/add/json")
                 .then().log().all()
                 .assertThat().statusCode(200)
                 .body("scope", equalTo("APP"))
                 .body("status", equalTo("OK"))
                 .header("Server", "Apache/2.4.52 (Ubuntu)");
+        //Add Place -> Update Place with New Address -> Get Place to validate if the New Address is present in the response
+
     }
 }
